5045d56b9ca1561152a4de11abbe77582acf0680
joat21 <eridel61@yandex.ru>
2024-11-01 14:12:07 +0500

    refactor: tariff page
    Reworked layout, without styles

--- src/pages/TariffPage/index.tsx
+++ src/pages/TariffPage/index.tsx
@@ -1,64 +1,72 @@
 import { FC } from 'react';
-import { Block, Button, Container, PageTitle } from '../../UI';
+import { Block, Button, Container } from '../../UI';
 import { useParams } from 'react-router-dom';
-import { ServiceIconsList } from '../../components/ServiceIconsList';
 import styles from './TariffPage.module.scss';
 import { useGetTariffQuery } from '../../services/tariffsApi';
-
-const unlimitedAppsIcons = {
-  unlimitedSocials: '../src/assets/img/services/socials.svg',
-  unlimitedVideo: '../src/assets/img/services/video.svg',
-  unlimitedMusic: '../src/assets/img/services/music.svg',
-};
-
-const extraServicesIcons = {
-  intercityCalls: '../src/assets/img/services/intercityCalls.svg',
-};
+import { useGetServicesDataQuery } from '../../services/servicesConfigApi';
 
 const TariffPage: FC = () => {
   const { id = '' } = useParams();
+  const { data: servicesData, isLoading: isSerivcesDataLoading } = useGetServicesDataQuery();
   const { data: tariff, isLoading } = useGetTariffQuery(id);
 
-  if (isLoading || !tariff) return 'Загрузка...';
+  if (isSerivcesDataLoading || !servicesData || isLoading || !tariff) return 'Загрузка...';
 
-  const { basicServices, unlimitedApps, extraServices } = tariff;
+  const { title, price, basicServices, unlimitedApps, extraServices } = tariff;
+  const basicServicesEntriesArray = Object.entries(basicServices);
+  const unlimitedAppsEntriesArray = Object.entries(unlimitedApps);
+  const extraServicesEntriesArray = Object.entries(extraServices);
 
   return (
-    <>
-      <PageTitle>Тариф {tariff.title}</PageTitle>
-      <Container>
-        <div className={styles.services}>
-          <Block>
-            <ul>
-              <li>{basicServices.internet} гб</li>
-              <li>{basicServices.minutes} мин</li>
-              <li>{basicServices.sms} смс</li>
-            </ul>
-          </Block>
-          {Object.values(unlimitedApps).some((item) => item) && (
-            <Block>
-              <span>Безлимиты</span>
-              <ServiceIconsList
-                services={unlimitedApps}
-                serviceIcons={unlimitedAppsIcons}
-              />
-            </Block>
-          )}
-          {Object.values(extraServices).some((item) => item) && (
-            <Block>
-              <span>Дополнительно</span>
-              <ServiceIconsList
-                services={extraServices}
-                serviceIcons={extraServicesIcons}
-              />
-            </Block>
-          )}
-        </div>
+    <Container>
+      <h1 className={styles.title}>{title}</h1>
+      <div className={styles.top}>
+        <ul className={styles.basic}>
+          {basicServicesEntriesArray.map(([name, amount]) => {
+            const basicServiceData = servicesData[0].basicServicesData[name];
+            return (
+              <li key={basicServiceData.id}>
+                <Block style={{ padding: '28px 60px' }}>
+                  {amount} {basicServiceData.measureUnit}
+                </Block>
+              </li>
+            );
+          })}
+        </ul>
         <Button className={styles.btn} to="/">
-          Подключить за {tariff.price} руб/мес
+          Купить за {price} руб/мес
         </Button>
-      </Container>
-    </>
+      </div>
+      <div className={styles.bottom}>
+        <h2>Включено в тариф</h2>
+        <ul className={styles.services}>
+          {unlimitedAppsEntriesArray.map(([name, value]) => {
+            if (!value) return null;
+            const unlimitedAppData = servicesData[0].unlimitedAppsData[name];
+            return (
+              <li key={unlimitedAppData.id}>
+                <Block>
+                  <span>{unlimitedAppData.label}</span>
+                  <img src={unlimitedAppData.imageUrl} alt={unlimitedAppData.label} />
+                </Block>
+              </li>
+            );
+          })}
+          {extraServicesEntriesArray.map(([name, value]) => {
+            if (!value) return null;
+            const extraServiceData = servicesData[0].extraServicesData[name];
+            return (
+              <li key={extraServiceData.id}>
+                <Block>
+                  <span>{extraServiceData.label}</span>
+                  <img src={extraServiceData.imageUrl} alt={extraServiceData.label} />
+                </Block>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </Container>
   );
 };
 
