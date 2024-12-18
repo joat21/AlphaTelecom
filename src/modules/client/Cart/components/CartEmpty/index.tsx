fe7e7ee0835ffada3bd6468a65454ced2deee4a8
c0ldhand <evgeny_nazarov@inbox.ru>
2024-12-18 10:47:41 +0500

    add display for cart-empty page

--- src/modules/client/Cart/components/CartEmpty/index.tsx
+++ src/modules/client/Cart/components/CartEmpty/index.tsx
@@ -1,9 +1,15 @@
 import styles from './CartEmpty.module.scss';
+import cartEmpty from '@assets/img/cart/cart-empty.svg';
+import { Button } from '@UI';
 
 export const CartEmpty = () => {
   return (
     <div className={styles.div}>
-      <h1>Корзина пустая</h1>
+      <img width="460" height="430" src={cartEmpty} alt="Корзина пустая"></img>
+      <p>В корзине ничего нет</p>
+      <Button className={styles.btn} to={'/tariffs'}>
+        Перейти к тарифам
+      </Button>
     </div>
   );
 };
